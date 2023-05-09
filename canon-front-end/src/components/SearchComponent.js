b4153b8580cff5e0b4b662944946d5024431a742
Temuulen <i427840>
2023-05-09 11:57:23 +0200

    V2

--- canon-front-end/src/components/SearchComponent.js
+++ canon-front-end/src/components/SearchComponent.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import './SearchComponent.css'; 
 
 function SearchComponent() {
   const [searchTerm, setSearchTerm] = useState('');
   const [filter, setFilter] = useState('errorid');
   const [results, setResults] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(`api`);
-      const data = await response.json();
-      const testSetNames = data.testbatch.testsets.map((testset) => testset.name);
-      setResults(testSetNames);
-    }
+  async function fetchData() {
+    const response = await fetch(`api`);
+    const data = await response.json();
+    const testSetNames = data.testbatch.testsets.map((testset) => testset.name);
+    setResults(testSetNames);
+  }
 
+  useEffect(() => {
     if (searchTerm !== '') {
       fetchData();
     }
   }, [searchTerm, filter]);
+
 
   function handleSearchTermChange(event) {
     setSearchTerm(event.target.value);
@@ -26,29 +28,38 @@ function SearchComponent() {
     setFilter(event.target.value);
   }
 
+  function handleSearchButtonClick() {
+    // call fetch data or any other search function here
+    // using the current values of searchTerm and filter
+    fetchData();
+  }
+
   return (
-    <div>
-      <label htmlFor="filter">Filter by:</label>
-      <select id="filter" value={filter} onChange={handleFilterChange}>
-        <option value="errorid">Error Id</option>
-        <option value="version">Version</option>
-        <option value="commitSHAL">Commit SHAL</option>
-      </select>
-      <br />
-      <label htmlFor="search">Search:</label>
-      <input type="text" id="search" value={searchTerm} onChange={handleSearchTermChange} />
-      <br />
-      {results.length > 0 ? (
-        <ul>
-          {results.map((result) => (
-            <li key={result}>{result}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No results found.</p>
-      )}
+<div className="container">
+  <div className="search-container">
+    <select id="filter" value={filter} onChange={handleFilterChange} className="select">
+      <option value="errorid">Error Id</option>
+      <option value="version">Version</option>
+      <option value="commitSHAL">Commit SHAL</option>
+    </select>
+    <div className="search-box">
+      <input type="text" id="search" value={searchTerm} onChange={handleSearchTermChange} className="input" />
+      <button onClick={handleSearchButtonClick} className="button">Search</button>
     </div>
+  </div>
+  {results.length > 0 ? (
+    <ul className="results-list">
+      {results.map((result) => (
+        <li key={result}>{result}</li>
+      ))}
+    </ul>
+  ) : (
+    <p className="no-results">No results found.</p>
+  )}
+</div>
   );
-}
+      
+} 
 
 export default SearchComponent;
+
--- canon-front-end/src/pages/SearchPage.js
+++ canon-front-end/src/pages/SearchPage.js
@@ -4,7 +4,6 @@ import SearchComponent from "../components/SearchComponent";
 function SearchPage(){
     return (
         <div>
-            <h1>This is the search page</h1>
             <SearchComponent />
         </div>
     );
