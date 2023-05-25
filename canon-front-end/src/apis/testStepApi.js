c106d49905b2c7765fbea66066b024b5015567c4
dktruman <57707222+dktruman@users.noreply.github.com>
2023-05-25 22:47:20 +0200

    Merged frontend of temuulen

--- canon-front-end/src/apis/testStepApi.js
+++ canon-front-end/src/apis/testStepApi.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/TestSteps";
 
-
-const testStepApi = {
-    getTestStepBySubTestID: () =>{
-        return axios.get(`http://localhost:8080/TestSteps`)
-    }
+const TestStepApi = {
+    getTestStepById: (testStepId) => axios.get(BASE_URL + "/" + testStepId)
 };
 
-export default testStepApi;
+export default TestStepApi;
