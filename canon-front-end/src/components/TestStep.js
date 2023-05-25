43a312850ba953373eb597cfc967241e0fd52cd8
Temuulen <i427840>
2023-05-25 11:23:38 +0200

    minor changes

--- canon-front-end/src/components/TestStep.js
+++ canon-front-end/src/components/TestStep.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import TestStepApi from '../apis/TestStepApi';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const TestStep = ({ id }) => {
   const [testStep, setTestStep] = useState(null);
@@ -9,21 +10,24 @@ const TestStep = ({ id }) => {
   useEffect(() => {
     const fetchTestStepById = async () => {
       try {
-        const response = await TestStepApi.getTestStepById(testStepId);
-        setTestStep(response);
+        const response =  await axios.get(`http://localhost:8080/TestSteps/${testStepId}`)
+        // const response = await TestStepApi.getTestStepById(testStepId);
+        setTestStep(response.data);
       } catch (error) {
         console.error('Error fetching test step:', error);
       }
     };
 
     fetchTestStepById();
-  }, [testStepId]);
+  }, [testStepId, testStep]);
 
   return (
     <div>
       <h1>Test Step Id: {testStepId}</h1>
       {testStep ? (
-        <TestStepComponent testStep={testStep} />
+        <><p>{testStep.description}</p>
+        <p>{testStep.testResult}</p>
+        <p>{testStep.id}</p></>
       ) : (
         <p>Loading ...</p>
       )}
