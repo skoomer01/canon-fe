3336039d3092125dc2c2659a197b68fb9fa6df58
dktruman <57707222+dktruman@users.noreply.github.com>
2023-06-11 22:49:48 +0200

    added stuff

--- canon-front-end/src/apis/testBatchApi.js
+++ canon-front-end/src/apis/testBatchApi.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const BATCH_BASE_URL = "http://localhost:8080/TestBatches";
+const BATCH_GET_BY_ID = "http://localhost:8080/TestBatches/individual";
 
 const BatchAPI = {
   
-  getAllTestSetsFromABatch: (id) => axios.get(`${BATCH_BASE_URL}/${id}`), 
+  getAllTestSetsFromABatch: (id) => axios.get(`${BATCH_BASE_URL}/${id}`),
+  getByID: (id) => axios.get(`${BATCH_GET_BY_ID}/${id}`)
 };
 
 export default BatchAPI;
