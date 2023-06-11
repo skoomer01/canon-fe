91f2c26bc8960eb0b09969538171f05d71c90961
Radu Andrei Popescu <radu.a.popescu10@gmail.com>
2023-06-11 19:33:05 +0200

    can now see private only if you are logged in

--- canon-front-end/src/apis/BranchApi.js
+++ canon-front-end/src/apis/BranchApi.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-
+import "../RequestInterceptor";
 const BRANCH_BASE_URL = "http://localhost:8080/Branches";
 
 const BranchAPI = {
   getAll: () => axios.get(BRANCH_BASE_URL),
   getAllTestBatchesFromABranch: (id) => axios.get(`${BRANCH_BASE_URL}/${id}`), 
+  getAllPrivate: () => axios.get(`${BRANCH_BASE_URL}/private`)
 };
 
 export default BranchAPI;
--- canon-front-end/src/components/branchSelectionCb.js
+++ canon-front-end/src/components/branchSelectionCb.js
@@ -3,8 +3,11 @@
     import "./branchSelectionCb.css";
     function BranchCombobox({ onSelectBranch }) {
         const [branches, setBranches] = useState([]);
+        const token = sessionStorage.getItem("token");
+        console.log(token); 
         useEffect(() => {
-            
+            if(token === null)
+            {
                 BranchAPI.getAll()
                 .then((response) => {
                     const branchList = response.data.branchList;
@@ -16,6 +19,21 @@
                     console.log("failed");
                     alert("failed");
                 })
+            }
+            else{
+                BranchAPI.getAllPrivate()
+                .then((response) => {
+                    const branchList = response.data.branchList;
+                    setBranches(branchList);
+                    console.log(response.data);
+                    console.log("Successfully");
+                })
+                .catch(function (error){
+                    console.log("failed");
+                    alert("failed");
+                })
+            }
+                
             
         },[]);
         const handleSelectBranch = (event) => {
