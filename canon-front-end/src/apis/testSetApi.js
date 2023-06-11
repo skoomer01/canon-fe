aa2f4108789349148d2032f89a46611d090b7a2c
dktruman <57707222+dktruman@users.noreply.github.com>
2023-06-11 23:12:16 +0200

    stuff

--- canon-front-end/src/apis/testSetApi.js
+++ canon-front-end/src/apis/testSetApi.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const TEST_BASE_URL = "http://localhost:8080/TestSets";
 const TESTSET_BASE_URL = "http://localhost:8080/TestSets";
 const TESTSET_FAILEDCOUNTER = "http://localhost:8080/TestSets/failedCounter";
-
-const TestAPI = {
-    getLatestTestSets: () => axios.get("http://localhost:8080/TestSets/latest"),
-    getAllTestsByTestSetId: (id) => axios.get(`${TESTSET_BASE_URL}/${id}`),
-    getFailedCounter: (id) => axios.get(`${TESTSET_FAILEDCOUNTER}/${id}`)
+const TestSetAPI = {
+  
+  getAllTestsByTestSetId: (id) => axios.get(`${TESTSET_BASE_URL}/${id}`), 
+  getFailedCounter: (id) => axios.get(`${TESTSET_FAILEDCOUNTER}/${id}`)
 };
 
-export default TestAPI;
+export default TestSetAPI;
