f59e5c6d99a248fd7eb0d7b18171f7aad74b0d40
Andrei-Alx <491992@student.fontys.nl>
2023-06-11 22:05:24 +0200

    homepage is working

--- canon-front-end/src/components/TestSet.js
+++ canon-front-end/src/components/TestSet.js
@@ -1,61 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import './TestSet.css';
-import { Card, CardBody, CardTitle} from 'reactstrap';
+import { Card, CardBody, CardTitle, Container } from 'reactstrap';
 import RegrTestApi from '../apis/regrTestApi';
 import andrei from '../apis/andrei';
-function TestSet(props) {
-const [tests, setTests] = useState([]);
-const [testsPerTestSet, setTestsPerTestSet] = useState([[]]);
 
-const fetchTestsByTestSetId = () => {
-  const updatedTestDatas = {};
-  console.log(props.testSets);
-  // props.testSets.forEach((testSet) => {
-  //   console.log(testSet.id);
-  //   andrei.testStuff(testSet.id)
-  //         .then((response) => {
-  //             console.log("response");
-  //             console.log(response);
-  //             const latestTestsData = response.data.latestTests;
-  //             console.log("LATEST TESTS DATA");
-  //             console.log(latestTestsData);
-  //             updatedTestDatas[testSet.id] = latestTestsData;
-  //             setTests(updatedTestDatas);
-  //         })
-  //         .catch((error) => console.log(error));
-  // });
-  props.testSets.forEach(testSet => {
-    andrei.getAllTestsByTestSetId(testSet.id)
-      .then((response) =>{
-        console.log("response");
-        console.log(response);
-        setTestsPerTestSet(response.data.latestTests);
-        tests[testSet.id]=testsPerTestSet;
-      })
-      .catch((error) => console.log(error));
-  });
-};
+function TestSet(props) {
+  const [tests, setTests] = useState([]);
+  const [testsPerTestSet, setTestsPerTestSet] = useState([[]]);
 
   useEffect(() => {
-    console.log("USE EFFECT");
     fetchTestsByTestSetId();
   }, [props.testSets]);
 
+  const fetchTestsByTestSetId = async () => {
+    const updatedTestDatas = {};
+
+    for (const testSet of props.testSets) {
+      try {
+        const response1 = await andrei.testStuff(testSet.id);
+        const latestTestsData = response1.data.latestTests;
+        updatedTestDatas[testSet.id] = latestTestsData;
+
+        const response2 = await andrei.getAllTestsByTestSetId(testSet.id);
+        const testsPerTestSet = response2.data.latestTests;
+        setTestsPerTestSet(testsPerTestSet);
+        tests[testSet.id] = testsPerTestSet;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    try {
+      setTests(updatedTestDatas);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="testset-page">
-      {props.testSets && props.testSets.map(testSet => (
-        <div className="testset-column" key={testSet.id}>
-          <h2>{testSet.name}&nbsp;</h2>
-          {/* {testSet && tests && tests[testSet.id].map((test) =>(
-              <Card>
-                <CardBody>
-                  <CardTitle>{test.testResult}</CardTitle>
-                </CardBody>
-              </Card>
-          ))} */}
-        </div>
-      ))}
-    </div>
+    <Container className="testset-page">
+      {props.testSets &&
+        props.testSets.map((testSet) => (
+          <div className="testset-column" key={testSet.id}>
+            <h2>{testSet.name}&nbsp;</h2>
+            {testSet &&
+              tests &&
+              tests[testSet.id] &&
+              tests[testSet.id].map((test) => (
+                <Card
+                  key={test.id}
+                  className={test.testResult.toString() === 'true' ? 'bg-success' : 'bg-danger'}
+                >
+                  <CardBody>
+                    <CardTitle>
+                      {test.testResult.toString() === 'true' ? 'Passed' : 'Failed'}
+                    </CardTitle>
+                  </CardBody>
+                </Card>
+              ))}
+          </div>
+        ))}
+    </Container>
   );
 }
 
--- canon-front-end/src/pages/HomePage.js
+++ canon-front-end/src/pages/HomePage.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import TestSet from "../components/TestSet";
 import testSetApi from "../apis/testSetApi";
 
-
 function HomePage(){
     const [testSets, setTestSets] = useState([]);
 
