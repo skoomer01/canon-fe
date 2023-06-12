aeff5871adfbc9aa33714110f0b7d9ee77f4dec9
dktruman <57707222+dktruman@users.noreply.github.com>
2023-06-12 23:08:23 +0200

    added the information of which route it has on multiple pages

--- canon-front-end/src/components/SubTestForm.js
+++ canon-front-end/src/components/SubTestForm.js
@@ -3,25 +3,61 @@ import subTestApi from "../apis/subTestApi";
 import testStepApi from "../apis/testStepApi";
 import { useNavigate } from "react-router-dom";
 import './SubTest.css';
+import testSetApi from "../apis/TestSetApi";
+import testBatchApi from "../apis/testBatchApi";
+import branchApi from "../apis/BranchApi";
 
 function SubTestForm({ subTestDetails }) {
     const [testSteps, setTestSteps] = useState([]);
-    const [subTest, setSubTest] = useState(null);
     const [error, setError] = useState(null);
     const navigate = useNavigate();
+    const [subTest, setSubTest] = useState(null);
+    const [test, setTest] = useState(null);
+    const [testSet, setTestSet] = useState(null);
+    const [testBatch, setTestBatch] = useState(null);
+    const [branch, setBranch] = useState(null);
 
     useEffect(() => {
         subTestApi.getSubTest(subTestDetails)
             .then(response => {
                 console.log(response.data);
                 setSubTest(response.data);
+                return response.data.testId;
             })
-            .catch(error => {
-                console.log(error);
-                setError(error);
-            });
-
-        testStepApi.getTestStepBySubTestID(subTestDetails)
+            .then((testId) => {
+                return testSetApi.getTestSet(testId);
+            })
+            .then((response) => {
+                console.log(response.data);
+                setTestSet(response.data);
+                return response.data.testSetId;
+            })
+            .then((testSetId) => {
+                return testSetApi.getTestSet(testSetId);
+            })
+            .then((response) => {
+                console.log(response.data);
+                setTestSet(response.data);
+                return response.data.testBatchId;
+            })
+            .then((testBatchId) => {
+                return testBatchApi.getByID(testBatchId);
+            })
+            .then((response) => {
+                console.log(response.data);
+                setTestBatch(response.data);
+                return response.data.branchId;
+            })
+            .then((branchId) => {
+                return branchApi.getBranchByID(branchId);
+            })
+            .then((response) => {
+                setBranch(response.data);
+                console.log(response.data);
+            })
+            .then(() => {
+                return testStepApi.getTestStepBySubTestID(subTestDetails);
+            })
             .then(testStepResponse => {
                 console.log(testStepResponse.data.testSteps);
                 setTestSteps(testStepResponse.data.testSteps);
@@ -36,16 +72,37 @@ function SubTestForm({ subTestDetails }) {
 
 
 
+
     const handleButtonClick = (testStepId) => {
         navigate(`/TestStep/${testStepId}`);
     };
 
     return (
         <div>
+            {branch == null ? (
+                <div>Nothing</div>
+            ) : (
+                <div>Branch: {branch.branchName}</div>
+            )}
+            {testBatch == null ? (
+                <div>Nothing</div>
+            ) : (
+                <div>Version: {testBatch.version}</div>
+            )}
+            {testSet == null ? (
+                <div>Nothing</div>
+            ) : (
+                <div>testSet: {testSet.name}</div>
+            )}
+            {test == null ? (
+                <div>Nothing</div>
+            ) : (
+                <div>Test: {test.testName}</div>
+            )}
             {subTest == null ? (
                 <div>Nothing</div>
             ) : (
-                <div>asd</div>
+                <div>Subtest: {subTest.subtestName}</div>
             )}
             <table>
                 <thead>
--- canon-front-end/src/components/TestStep.js
+++ canon-front-end/src/components/TestStep.js
@@ -2,14 +2,83 @@ import React, { useState, useEffect } from 'react';
 import TestStepApi from '../apis/testStepApi';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import subTestApi from "../apis/subTestApi";
+import testSetApi from "../apis/TestSetApi";
+import testBatchApi from "../apis/testBatchApi";
+import branchApi from "../apis/BranchApi";
+import testStepApi from "../apis/testStepApi";
+import testApi from "../apis/testApi";
 
 const TestStep = ({ id }) => {
   const [testStep, setTestStep] = useState(null);
-  const { id: testStepId } = useParams();
+  const { id : testStepId } = useParams();
+  const [subTest, setSubTest] = useState(null);
+  const [test, setTest] = useState(null);
+  const [testSet, setTestSet] = useState(null);
+  const [testBatch, setTestBatch] = useState(null);
+  const [branch, setBranch] = useState(null);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    testStepApi.getTestStepById(testStepId)
+        .then(response => {
+          console.log(response.data);
+          setTestStep(response.data);
+          return response.data.subTestId;
+        })
+        .then((subTestId) => {
+          return subTestApi.getSubTest(subTestId);
+        })
+        .then(response => {
+          console.log(response.data);
+          setSubTest(response.data);
+          return response.data.testId;
+        })
+        .then((testId) => {
+          return testApi.getTest(testId);
+        })
+        .then((response) => {
+          console.log(response.data);
+          setTest(response.data);
+          return response.data.testSetId;
+        })
+        .then((testSetId) => {
+          return testSetApi.getTestSet(testSetId);
+        })
+        .then((response) => {
+          console.log(response.data);
+          setTestSet(response.data);
+          return response.data.testBatchId;
+        })
+        .then((testBatchId) => {
+          return testBatchApi.getByID(testBatchId);
+        })
+        .then((response) => {
+          console.log(response.data);
+          setTestBatch(response.data);
+          return response.data.branchId;
+        })
+        .then((branchId) => {
+          return branchApi.getBranchByID(branchId);
+        })
+        .then((response) => {
+          setBranch(response.data);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+          setError(error);
+        });
+  }, []);
+
+
+
 
   useEffect(() => {
     const fetchTestStepById = async () => {
       try {
+        // const response =  await axios.get(`http://localhost:8080/TestSteps/${testStepId}`)
         const response = await TestStepApi.getTestStepById(testStepId);
         setTestStep(response.data);
       } catch (error) {
@@ -20,40 +89,66 @@ const TestStep = ({ id }) => {
     fetchTestStepById();
   }, [testStepId, testStep]);
 
-  const getTestResultStyle = () => {
-    if (testStep && !testStep.testResult) {
-      return { backgroundColor: 'red' };
-    } else if (testStep && testStep.testResult) {
-      return { backgroundColor: 'green' };
-    }
-    return {};
-  };
-
   return (
     <div>
+      {branch == null ? (
+          <div>Nothing</div>
+      ) : (
+          <div>Branch: {branch.branchName}</div>
+      )}
+      {testBatch == null ? (
+          <div>Nothing</div>
+      ) : (
+          <div>Version: {testBatch.version}</div>
+      )}
+      {testSet == null ? (
+          <div>Nothing</div>
+      ) : (
+          <div>testSet: {testSet.name}</div>
+      )}
+      {test == null ? (
+          <div>Nothing</div>
+      ) : (
+          <div>Test: {test.testName}</div>
+      )}
+      {subTest == null ? (
+          <div>Nothing</div>
+      ) : (
+          <div>Subtest: {subTest.subtestName}</div>
+      )}
+      {subTest == null ? (
+          <div>Nothing</div>
+      ) : (
+          <div>TestStep: {testStep.testStepName}</div>
+      )}
+
+
+
+
       {testStep ? (
+
         <table>
           <thead>
-            <tr>
-              <th>Test Step Name</th>
-              {/* <th>Sub Test ID</th> */}
-              <th>Test Result</th>
-              <th>Description</th>
-              <th>Error ID</th>
-            </tr>
+           <tr>
+             <th>ID</th>
+             <th>Test Step Name</th>
+             <th>Sub Test ID</th>
+             <th>Test Result</th>
+             <th>Description</th>
+             <th>Error ID</th>
+           </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>{testStep.testStepName}</td>
-              {/* <td>{testStep.subTestId}</td> */}
-              <td style={getTestResultStyle()}>
-                {testStep.testResult ? 'Passed' : 'Failed'}
-              </td>
-              <td>{testStep.description}</td>
-              <td>{testStep.errorid}</td>
-            </tr>
+          <tr>
+            <td>{testStep.id}</td>
+            <td>{testStep.testStepName}</td>
+            <td>{testStep.subTestId}</td>
+            <td>{testStep.testResult ? "Passed" : "Failed"}</td>
+            <td>{testStep.description}</td>
+            <td>{testStep.errorid}</td>
+          </tr>
           </tbody>
-        </table>
+          </table>
       ) : (
         <p>Loading ...</p>
       )}
