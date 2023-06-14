c7a9e0da571ffc18243452ee86265eb7ee6b7698
dktruman <57707222+dktruman@users.noreply.github.com>
2023-06-14 20:07:39 +0200

    fixed subtests page

--- canon-front-end/src/apis/testApi.js
+++ canon-front-end/src/apis/testApi.js
@@ -6,6 +6,9 @@ const TestApi = {
     getTest: (testDetails) => {
         return axios.get(`http://localhost:8080/Tests/${testDetails}`)
     },
+    getTestById: (testDetails) => {
+        return axios.get(`http://localhost:8080/Tests/${testDetails}`)
+    },
 };
 
 export default TestApi;
