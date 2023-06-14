7a882cf3bf36b7285f764f028f5407041c64c56e
Andrei-Alx <491992@student.fontys.nl>
2023-06-14 18:33:46 +0200

    Homepage works

--- canon-front-end/src/components/TestSet.js
+++ canon-front-end/src/components/TestSet.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './TestSet.css';
 import { Card, CardBody, CardTitle, Container } from 'reactstrap';
 import TestSetApi from '../apis/TestSetApi';
+import { Link } from 'react-router-dom';
 
 function TestSet(props) {
   const [tests, setTests] = useState([]);
   const [testsPerTestSet, setTestsPerTestSet] = useState([[]]);
+  const [failedTestCounts, setFailedTestCounts] = useState({});
 
   useEffect(() => {
     fetchTestsByTestSetId();
@@ -13,6 +15,7 @@ function TestSet(props) {
 
   const fetchTestsByTestSetId = async () => {
     const updatedTestDatas = {};
+    const updatedFailedTestCounts = {};
 
     for (const testSet of props.testSets) {
       try {
@@ -24,6 +27,9 @@ function TestSet(props) {
         const testsPerTestSet = response2.data.latestTests;
         setTestsPerTestSet(testsPerTestSet);
         tests[testSet.id] = testsPerTestSet;
+
+        const failedCount = await fetchFailedTestStepCount(testSet.id);
+        updatedFailedTestCounts[testSet.id] = failedCount;
       } catch (error) {
         console.log(error);
       }
@@ -31,8 +37,19 @@ function TestSet(props) {
 
     try {
       setTests(updatedTestDatas);
+      setFailedTestCounts(updatedFailedTestCounts);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const fetchFailedTestStepCount = async (testId) => {
+    try {
+      const response = await TestSetApi.getFailedCounter(testId); // Use TestSetAPI.getFailedCounter method
+      return response.data.failedCounter;
+    } catch (error) {
+      console.error('Error fetching failed test step count:', error);
+      return 1;
     }
   };
 
@@ -42,18 +59,25 @@ function TestSet(props) {
         props.testSets.map((testSet) => (
           <div className="testset-column" key={testSet.id}>
             <h2>{testSet.name}&nbsp;</h2>
+            {testSet && tests && tests[testSet.id] && (
+              <div>Total Failed Tests: {failedTestCounts[testSet.id]}</div>
+            )}
             {testSet &&
               tests &&
               tests[testSet.id] &&
               tests[testSet.id].map((test) => (
-                <Card
-                  key={test.id}
-                  //className={test.testName.toString() === 'true' ? 'bg-success' : 'bg-danger'}
-                >
+                <Card key={test.id}>
                   <CardBody>
-                    <CardTitle>
-                      {test.testName.toString() /*=== 'true' ? 'Passed' : 'Failed'*/}
-                    </CardTitle>
+                    <Link key={test.id} to={`/testdetailspage/${test.id}`} testDetails={test.id} className='pageLink'>
+                      <div key={test.id} className={test.failedCount > 0 ? 'bg-danger' : 'bg-success'}>
+                        {test.failedCount > 0 && (
+                          <div className="failed-step-count">
+                            {test.failedCount}
+                          </div>
+                        )}
+                      </div>
+                    </Link>
+                    <CardTitle>{test.testName.toString()}</CardTitle>
                   </CardBody>
                 </Card>
               ))}
@@ -62,5 +86,4 @@ function TestSet(props) {
     </Container>
   );
 }
-
 export default TestSet;
