b80660a4161bb397102c472602814ac4f5294b9b
Andrei-Alx <491992@student.fontys.nl>
2023-06-20 03:24:11 +0200

    Homepage working correctly

--- canon-front-end/src/components/TestSet.js
+++ canon-front-end/src/components/TestSet.js
@@ -1,28 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import './TestSet.css';
-import { Card, CardBody, CardTitle, Container } from 'reactstrap';
-import TestSetApi from '../apis/TestSetApi';
+// import mockData from '../mockdata/mock-data2.json';
+// import BranchAPI from "../apis/BranchApi";
+// import TestBatchAPI from "../apis/testBatchApi"
+import TestSetAPI from "../apis/TestSetApi"
+// import { Card, CardBody, CardTitle, Container } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 function TestSet(props) {
-  const [tests, setTests] = useState([]);
-  const [testsPerTestSet, setTestsPerTestSet] = useState([[]]);
-  const [failedTestCounts, setFailedTestCounts] = useState({});
+  // const [testBatches, setTestBatches] = useState([]);
+  // const [testSets, setTestSets] = useState([]);
+  const [testData, setTestData] = useState([]);
   const [updatedTestSets, setUpdatedTestSets] = useState([]);
+  // const [updatedTestBatches, setUpdatedTestBatches] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       if (selectedBranchId !== 0) {
+  //         const response = await BranchAPI.getAllTestBatchesFromABranch(selectedBranchId);
+  //         setTestBatches(response.data.testBatchList);
+  //       } else {
+  //         setTestBatches([]); // Reset test batches when no branch is selected
+  //       }
+  //     } catch (error) {
+  //       console.log("Failed to fetch test batches:", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [selectedBranchId]);
+
+  // useEffect(() => {
+  //   const fetchTestSets = async () => {
+  //     const testSetsPromises = testBatches.map((testBatch) =>
+  //       TestBatchAPI.getAllTestSetsFromABatch(testBatch.id)
+  //     );
+
+  //     try {
+  //       const testSetsResponses = await Promise.all(testSetsPromises);
+  //       const testSetsData = testSetsResponses.map((response) => response.data.testSetList);
+  //       setTestSets(testSetsData.flat());
+  //     } catch (error) {
+  //       console.error('Error fetching test sets:', error);
+  //     }
+  //   };
+
+  //   fetchTestSets();
+  // }, [testBatches]);
 
   useEffect(() => {
-    fetchTestsByTestSetId();
+    const fetchTests = async () => {
+      const testSetIds = props.testSets.map((testSet) => testSet.id);
+      const testsPromises = testSetIds.map(async (testSetId) => {
+        const response = await TestSetAPI.getAllTestsByTestSetId(testSetId);
+        return response.data.regressionTests;
+      });
+
+      try {
+        const testsResponses = await Promise.all(testsPromises);
+        const testsData = testsResponses.flat();
+        setTestData(testsData);
+      } catch (error) {
+        console.error('Error fetching tests:', error);
+      }
+    };
+
+    fetchTests();
   }, [props.testSets]);
-
-  useEffect(() => {
-    if (tests.length > 0) {
-      updateTestSetsWithTestsAndFailedCount();
-    }
-  }, [tests]);
-
-  useEffect(() => {
-    console.log("kkk" + JSON.stringify(updatedTestSets));
-  },[updatedTestSets])
 
   const updateTestSetsWithTestsAndFailedCount = async () => {
     const updatedTestSets = props.testSets.map((testSet) => ({
@@ -30,8 +74,8 @@ function TestSet(props) {
       regressionTests: [],
     }));
 
-    for (let i = 0; i < tests.length; i++) {
-      const test = tests[i];
+    for (let i = 0; i < testData.length; i++) {
+      const test = testData[i];
       const testSetIndex = updatedTestSets.findIndex((testSet) => testSet.id === test.testSetId);
       if (testSetIndex !== -1) {
         const failedCount = await fetchFailedTestStepCount(test.id);
@@ -45,39 +89,47 @@ function TestSet(props) {
     setUpdatedTestSets(updatedTestSets);
   };
 
-  const fetchTestsByTestSetId = async () => {
-    const updatedTestDatas = {};
-    const updatedFailedTestCounts = {};
-
-    for (const testSet of props.testSets) {
-      try {
-        const response1 = await TestSetApi.getLatestTestsByTestSet(testSet.id);
-        const latestTestsData = response1.data.latestTests;
-        updatedTestDatas[testSet.id] = latestTestsData;
-
-        const response2 = await TestSetApi.getAllTestsByTestSetId(testSet.id);
-        const testsPerTestSet = response2.data.latestTests;
-        setTestsPerTestSet(testsPerTestSet);
-        tests[testSet.id] = testsPerTestSet;
-
-        const failedCount = await fetchFailedTestStepCount(testSet.id);
-        updatedFailedTestCounts[testSet.id] = failedCount;
-      } catch (error) {
-        console.log(error);
-      }
+  useEffect(() => {
+    if (testData.length > 0) {
+      updateTestSetsWithTestsAndFailedCount();
     }
+  }, [testData]);
 
-    try {
-      setTests(updatedTestDatas);
-      setFailedTestCounts(updatedFailedTestCounts);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // useEffect(() => {
+  //   console.log("kkk" + JSON.stringify(updatedTestSets));
+  // }, [updatedTestSets])
+
+  // const updateTestBatchesWithTestSets = () => {
+  //   const updatedTestBatches = testBatches.map((testBatch) => ({
+  //     ...testBatch,
+  //     testSets: [],
+  //   }));
+
+  //   updatedTestSets.forEach((testSet) => {
+  //     const testBatchIndex = updatedTestBatches.findIndex(
+  //       (testBatch) => testBatch.id === testSet.testBatchId
+  //     );
+  //     if (testBatchIndex !== -1) {
+  //       updatedTestBatches[testBatchIndex].testSets.push(testSet);
+  //     }
+  //   });
+
+  //   setUpdatedTestBatches(updatedTestBatches);
+  // };
+
+  // useEffect(() => {
+  //   if (updatedTestSets.length > 0) {
+  //     updateTestBatchesWithTestSets();
+  //   }
+  // }, [updatedTestSets, testBatches]);
+
+  // useEffect(() => {
+  //   console.log(JSON.stringify(updatedTestBatches));
+  // }, [updatedTestBatches]);
 
   const fetchFailedTestStepCount = async (testId) => {
     try {
-      const response = await TestSetApi.getFailedCounter(testId); // Use TestSetAPI.getFailedCounter method
+      const response = await TestSetAPI.getFailedCounter(testId); // Use TestSetAPI.getFailedCounter method
       return response.data.failedCounter;
     } catch (error) {
       console.error('Error fetching failed test step count:', error);
@@ -85,37 +137,60 @@ function TestSet(props) {
     }
   };
 
+  // if (testBatches.length === 0) {
+  //   return <div>Loading...</div>;
+  // }
   return (
-    <Container className="testset-page">
-      {props.testSets &&
-        props.testSets.map((testSet) => (
-          <div className="testset-column" key={testSet.id}>
-            <h2>{testSet.name}&nbsp;</h2>
-            {testSet && tests && tests[testSet.id] && (
-              <h5>Total Failed Tests: {failedTestCounts[testSet.id]}</h5>
-            )}
-            {testSet &&
-              tests &&
-              tests[testSet.id] &&
-              tests[testSet.id].map((test) => (
-                <Card key={test.id}>
-                  <CardBody>
-                    <Link key={test.id} to={`/testdetailspage/${test.id}`} testDetails={test.id} className='pageLink'>
-                      <div key={test.id} className={test.failedCount > 0 ? 'bg-danger' : 'bg-success'}>
-                        {test.failedCount > 0 && (
-                          <div className="failed-step-count">
-                            {test.failedCount}
-                          </div>
-                        )}
-                      </div>
-                    </Link>
-                    <CardTitle>{test.testName.toString()}</CardTitle>
-                  </CardBody>
-                </Card>
+    <div className="test-batch">
+      <table>
+        <thead>
+          <tr>
+            {updatedTestSets.map((ts) => (
+              <th key={ts.id}>Test set name: {ts.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            <tr key={updatedTestSets.id}>
+              {updatedTestSets.map((testSet) => (
+                <td key={testSet.id}>
+                  <div className="test-cont">
+                    {testSet.regressionTests.map((test) => (
+                      <Link
+                        key={test.id}
+                        to={`/testdetailspage/${test.id}`}
+                        testDetails={test.id}
+                        style={{ textDecoration: 'none' }}
+                      >
+                        <div
+                          key={test.id}
+                          className={
+                            test.failedCount > 0
+                              ? 'test-fail'
+                              : 'test-pass'
+                          }
+                        >
+                          {test.failedCount > 0 && (
+                            <div className="failed-step-count">
+                              {test.failedCount}
+                            </div>
+                          )}
+                          {test.failedCount === 0 && (
+                            <div className="checkmark">&#x2713;</div>
+                          )}
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                </td>
               ))}
-          </div>
-        ))}
-    </Container>
+            </tr>
+          }
+        </tbody>
+      </table>
+    </div>
   );
 }
+
 export default TestSet;
